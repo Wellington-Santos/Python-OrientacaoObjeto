f17ef50060801ba173f5b9cd306006f166daea54
Wellington <wellington.september@gmail.com>
2023-02-14 02:37:42 -0300

    Refatorando alguns metodos e criando alguns para consultas rapidas

--- conta.py
+++ conta.py
@@ -16,8 +16,36 @@ class Conta:
         self.__saldo += valor
 
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__disponivel_saque(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
+    def __disponivel_saque(self, valor):
+        valor_disponivel = self.__saldo + self.limite
+        return valor <= valor_disponivel
+
+    # Adição de um metodo para realizar transferências entre contas instânciadas em memória, sem redundância de código e o mais simples possível
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    # Adição dos metodos Get e Set para realizar consultas simples e alteração no limite da conta
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    # Usando os termos Property e Setter nos metódos de limites pois e mais facil e compreensivel de entender o código e evita de re-criar código desnecessário
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def saldo(self, valor):
+        self.__limite = valor
+
+    @staticmethod
+    def codigo_banco(self):
+        return "001"
