9841d6a20a88a0926ad8eee5971949cd2c14d7fb
Wellington <wellington.september@gmail.com>
2023-02-14 01:24:11 -0300

    Commit inicial do projeto de Orientação a Objeto

---
+++ conta.py
@@ -0,0 +1,23 @@
+class Conta:
+
+    # Inicialização da classe Conta, nela teremos alguns metodos básicos para realizar algumas ações simples com conceitos de Orientações a Objetos
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do Titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
