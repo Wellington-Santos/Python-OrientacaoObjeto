f5f25c4207cd14c0ab6dea03aba822cb5fd2b90a
Wellington <wellington.september@gmail.com>
2023-02-15 02:51:26 -0300

    Refatoração no Código e criação de novas funcionalidasdes

--- Curso-02/Model/model.py
+++ Curso-02/Model/model.py
@@ -1,5 +1,4 @@
 # Definição da Classe Pai, que terá atributos e métodos herdados pelas classes filhas 
-
 class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
@@ -25,7 +24,6 @@ class Programa:
     def __str__(self):
         return f"{self._nome} - {self.ano} - {self._likes} Likes"
 
-
 # Definição das classes filhas e seus atributos particulares
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
@@ -36,7 +34,6 @@ class Filme(Programa):
     def __str__(self):
         return f"{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes"
 
-
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
@@ -45,18 +42,51 @@ class Serie(Programa):
     def __str__(self):
         return f"{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes"
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+    
 
 filme = Filme("top gun: Maverick", 2022, 160)
-serie = Serie("gothan", 2017, 8)
+filme2 = Filme("todo mundo em pânico", 1998, 100)
+serie = Serie("gothan", 2016, 8)
+serie2 = Serie("Demolidor", 2018, 6)
 
 filme.dar_like()
 filme.dar_like()
+
+filme2.dar_like()
+filme2.dar_like()
+filme2.dar_like()
 
 serie.dar_like()
 
-playlist = [filme, serie]
+serie2.dar_like()
+serie2.dar_like()
+serie2.dar_like()
 
-for programa in playlist:
 
+
+playlist = [filme, serie, filme2, serie2]
+
+programacao = Playlist("Programação Fim de Semana", playlist)
+
+print(f"Tamanho da playlist {len(playlist)}")
+
+for programa in programacao:
     # A partir do momento que já tenho um método para realizar interpretação textual da classe declarado no objeto, eu posso simplesmente dar um print nele, que ele irá trazer as informações do objeto
     print(programa)
+
