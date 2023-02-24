db32df971b3632bcebfdc8038deff1dad05d926c
Wellington <wellington.september@gmail.com>
2023-02-24 01:34:39 -0300

    Realizando fatiamento das informações de um URL

--- Curso-03/main.py
+++ Curso-03/main.py
@@ -1,13 +1,27 @@
 # Autor: Wellington Santos - 24/02/2023 00:41
 # Começando a trabalhar com extrações de informações de strings e URLs
 
-# url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 
-print(url)
+# Separa a URL base e os parametros
+indice_interrogacao = url.find('?')
+url_base = url[: indice_interrogacao]
+url_parametros = url[indice_interrogacao + 1 :]
 
-url_base = url[0:19]
-url_parametros = url[20:36]
+# Busca o valor de um parâmetro
+# parametro_busca = 'moedaOrigem'
+parametro_busca = 'moedaDestino'
 
-print(url_base)
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+
+indice_e_comercial = url_parametros.find('&', indice_valor)
+
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+
 print(url_parametros)
+print(valor)
